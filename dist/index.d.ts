295b589f8d31431ed74e4d1b5e3f60135c2a850f
Kitryn <snowfennard@gmail.com>
2021-04-05 03:38:09 +0800

    update exports

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 export { GetNewToken } from './GetNewToken';
 export { GSheetsInterface } from './GSheetsInterface';
 export { SellSheet } from './SellSheet';
+export { SellAssetRow, GApiError } from './types';
--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.SellSheet = exports.GSheetsInterface = exports.GetNewToken = void 0;
+exports.GApiError = exports.SellAssetRow = exports.SellSheet = exports.GSheetsInterface = exports.GetNewToken = void 0;
 var GetNewToken_1 = require("./GetNewToken");
 Object.defineProperty(exports, "GetNewToken", { enumerable: true, get: function () { return GetNewToken_1.GetNewToken; } });
 var GSheetsInterface_1 = require("./GSheetsInterface");
 Object.defineProperty(exports, "GSheetsInterface", { enumerable: true, get: function () { return GSheetsInterface_1.GSheetsInterface; } });
 var SellSheet_1 = require("./SellSheet");
 Object.defineProperty(exports, "SellSheet", { enumerable: true, get: function () { return SellSheet_1.SellSheet; } });
+var types_1 = require("./types");
+Object.defineProperty(exports, "SellAssetRow", { enumerable: true, get: function () { return types_1.SellAssetRow; } });
+Object.defineProperty(exports, "GApiError", { enumerable: true, get: function () { return types_1.GApiError; } });
